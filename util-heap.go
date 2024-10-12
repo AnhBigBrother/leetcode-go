5164372eff41df598079a9003f731f1a75a6405e
anh bigbruhh <anh.bigbrother@gmail.com>
2024-10-12 20:23:32 +0700

    Oct 12, 2024

--- util-heap.go
+++ util-heap.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"container/heap"
+)
+
 type IntHeap []int
 
 func (h IntHeap) Len() int           { return len(h) }
@@ -16,4 +20,47 @@ func (h *IntHeap) Pop() any {
 	x := old[n-1]
 	*h = old[0 : n-1]
 	return x
+}
+
+type Item struct {
+	value    int
+	priority int
+	index    int
+}
+
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].priority > pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x any) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() any {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil
+	item.index = -1
+	*pq = old[0 : n-1]
+	return item
+}
+
+func (pq *PriorityQueue) update(item *Item, value int, priority int) {
+	item.value = value
+	item.priority = priority
+	heap.Fix(pq, item.index)
 }
