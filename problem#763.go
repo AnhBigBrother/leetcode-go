71bbf7c9c437fd3ea51ac9e0d64693e495ab7003
anh.bigbro <anh.bigbrother@gmail.com>
2025-04-14 15:36:46 +0700

    Apr 14, 2025

--- problem#763.go
+++ problem#763.go
@@ -1,38 +1,23 @@
 package main
 
-import "slices"
-
 func partitionLabels(s string) []int {
-	mp := map[rune][]int{}
+	lastExist := [26]int{}
 	for i, c := range s {
-		if mp[c] == nil {
-			mp[c] = []int{i, i}
-		} else {
-			mp[c][1] = i
-		}
+		lastExist[int(c)-97] = i
 	}
-	partitions := [][]int{}
-	for _, p := range mp {
-		partitions = append(partitions, p)
-	}
-	slices.SortFunc(partitions, func(p1, p2 []int) int {
-		if p1[0] != p2[0] {
-			return p1[0] - p2[0]
-		} else {
-			return p1[1] - p2[1]
-		}
-	})
+	last := 0
+	count := 0
 	ans := []int{}
-	w := partitions[0]
-	for i := 1; i < len(partitions); i++ {
-		p := partitions[i]
-		if p[0] < w[1] {
-			w[1] = max(w[1], p[1])
-		} else {
-			ans = append(ans, w[1]-w[0]+1)
-			w = p
+	for i, c := range s {
+		count++
+		le := lastExist[int(c)-97]
+		if le > last {
+			last = le
+		}
+		if i == last {
+			ans = append(ans, count)
+			count = 0
 		}
 	}
-	ans = append(ans, w[1]-w[0]+1)
 	return ans
 }
