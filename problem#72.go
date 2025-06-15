05b173fabc6943cd3d84835ec01568d86e9207a9
anh.bigbro <anh.bigbrother@gmail.com>
2025-06-16 02:35:15 +0700

    Jun 16, 2025

--- problem#72.go
+++ problem#72.go
@@ -2,14 +2,14 @@ package main
 
 func minDistance(word1 string, word2 string) int {
 	m, n := len(word1), len(word2)
-	dp := [][]int{}
-	for i := 0; i < m+1; i++ {
+	dp := [][]int{} // dp[i][j] = minDistance(word1[:i], word2[:j])
+	for range m + 1 {
 		dp = append(dp, make([]int, n+1))
 	}
-	for i := 0; i <= m; i++ {
+	for i := range m + 1 {
 		dp[i][0] = i
 	}
-	for j := 0; j <= n; j++ {
+	for j := range n + 1 {
 		dp[0][j] = j
 	}
 	for i := 1; i <= m; i++ {
