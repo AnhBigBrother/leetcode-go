ce4ed7d36e51bdaf94706f59864849c32dc711c5
anh.bigbro <anh.bigbrother@gmail.com>
2025-07-05 13:58:23 +0700

    Jul 5, 2025

--- problem#188.go
+++ problem#188.go
@@ -1,67 +1,20 @@
 package main
 
-type item struct {
-	start int
-	end   int
-}
-
-func maxProfit(k int, prices []int) int {
+func maxProfit188(k int, prices []int) int {
 	n := len(prices)
-	arr := []item{}
-	buy, sell := 0, 0
+	buy := make([]int, k)
+	sell := make([]int, k)
+	for i := range k {
+		buy[i] = -prices[0]
+	}
 	for i := 1; i < n; i++ {
-		for i < n && prices[i-1] >= prices[i] {
-			buy = i
-			i++
+		buy[0] = max(buy[0], -prices[i])
+		for j := 1; j < k; j++ {
+			buy[j] = max(buy[j], sell[j-1]-prices[i])
 		}
-		for i < n && prices[i-1] <= prices[i] {
-			sell = i
-			i++
-		}
-		if buy < sell && prices[buy] < prices[sell] {
-			arr = append(arr, item{
-				start: prices[buy],
-				end:   prices[sell],
-			})
-			buy = i
-			sell = i
+		for j := range k {
+			sell[j] = max(sell[j], buy[j]+prices[i])
 		}
 	}
-	for len(arr) > k {
-		minProfit := arr[0].end - arr[0].start
-		minDuration := 10001
-		for i := 1; i < len(arr); i++ {
-			minProfit = min(minProfit, arr[i].end-arr[i].start)
-			if arr[i].start < arr[i-1].end {
-				minDuration = min(minDuration, arr[i-1].end-arr[i].start)
-			}
-		}
-		newArr := []item{}
-		flag := true
-		if minDuration < minProfit {
-			newArr = append(newArr, arr[0])
-			for i := 1; i < len(arr); i++ {
-				if flag && arr[i-1].end-arr[i].start == minDuration {
-					flag = false
-					newArr[i-1].end = arr[i].end
-					continue
-				}
-				newArr = append(newArr, arr[i])
-			}
-		} else {
-			for _, a := range arr {
-				if flag && a.end-a.start == minProfit {
-					flag = false
-					continue
-				}
-				newArr = append(newArr, a)
-			}
-		}
-		arr = newArr
-	}
-	ans := 0
-	for _, x := range arr {
-		ans += x.end - x.start
-	}
-	return ans
+	return sell[k-1]
 }
